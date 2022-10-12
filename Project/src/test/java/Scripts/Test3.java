d40c596cb130f6b2fd1b82a5b8ff96122c72b0e9
MY LAPPY HOUSE <MY LAPPY HOUSE@DESKTOP-QAFE254>
2022-10-12 14:44:42 +0530

    Jenkins_Trial2

---
+++ Project/src/test/java/Scripts/Test3.java
@@ -0,0 +1,14 @@
+package Scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test3 {
+
+	
+	@Test
+	public void tc1() {
+		
+		Reporter.log("hello Pythonselenium",true );
+	}
+}
